e82c8f53de8115e046b73db912f9c58285432b55
fsaythanry <aldyzkobf@gmail.com>
2023-01-12 22:09:34 +0800

    styling detail page

--- pages/coffee-store/[id].jsx
+++ pages/coffee-store/[id].jsx
@@ -1,45 +1,80 @@
-import Error from 'next/error';
 import { useRouter } from 'next/router';
-import React from 'react';
-import coffeeStores from '../../data/coffee-store.json';
+import Link from 'next/link';
+import Head from 'next/head';
+import Image from 'next/image';
+import cls from 'classnames';
+import coffeeStoresData from '../../data/coffee-store.json';
+import styles from '../../styles/coffee-store.module.scss';
 
 export async function getStaticProps(staticProps) {
-    const { params } = staticProps;
-    const coffeeStore = coffeeStores.find(
-        ({ id }) => id.toString() === params.id
-    );
-
-    if (!coffeeStore) return { notFound: true };
-
-    return { props: { coffeeStore } };
+    const params = staticProps.params;
+    return {
+        props: {
+            coffeeStore: coffeeStoresData.find((coffeeStore) => {
+                return coffeeStore.id.toString() === params.id; //dynamic id
+            }),
+        },
+    };
 }
 
 export function getStaticPaths() {
+    const paths = coffeeStoresData.map((coffeeStore) => {
+        return {
+            params: {
+                id: coffeeStore.id.toString(),
+            },
+        };
+    });
     return {
-        paths: [{ params: { id: '0' } }, { params: { id: '1' } }],
+        paths,
         fallback: true,
     };
 }
 
-export default function CoffeeStoreDetail(props) {
-    const { coffeeStore } = props;
+const CoffeeStore = (props) => {
     const router = useRouter();
-    const {
-        isFallback,
-        query: { id },
-    } = router;
-
-    if (isFallback) {
-        return <p>Loading</p>;
+    if (router.isFallback) {
+        return <div>Loading...</div>;
     }
 
-    return (
-        <div>
-            <h1>
-                {coffeeStore.name} {id}
-            </h1>
+    const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
 
-            <p>{coffeeStore.address}</p>
+    const handleUpvoteButton = () => {};
+
+    return (
+        <div className={styles.layout}>
+            <Head>
+                <title>{name}</title>
+            </Head>
+
+            <div className={styles.container}>
+                <div className={styles.backToHomeLink}>
+                    <Link href="/">Back to home</Link>
+                </div>
+
+                <div className={styles.nameWrapper}>
+                    <h1>{name}</h1>
+                </div>
+
+                <div className={styles.storeImgWrapper}>
+                    <Image src={imgUrl} width={600} height={360} alt={name} />
+                </div>
+
+                <div className={`glass-no-hover ${styles.card}`}>
+                    <p className={styles.text}>{address}</p>
+                    <p className={styles.text}>{neighbourhood}</p>
+                    <p className={styles.text}>1</p>
+
+                    <button
+                        className={styles.upvoteButton}
+                        onClick={handleUpvoteButton}
+                    >
+                        Up vote!
+                    </button>
+                </div>
+            </div>
         </div>
     );
-}
+};
+
+export default CoffeeStore;
